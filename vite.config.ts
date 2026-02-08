6daede428cd7886fd3a8a032c06160bbef0a1bd4
RAY-266 <madzim637@gmail.com>
2026-02-08 19:20:39 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, ".", "");
-
-  return {
-    plugins: [react()],
-    base: "/",
-    server: {
-      port: 3000,
-      host: true
-    },
-    define: {
-      "process.env.API_KEY": JSON.stringify(env.API_KEY)
-    }
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: "/"
 });
+
